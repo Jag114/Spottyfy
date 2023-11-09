f36bded621ec216612f7ad5bfff8d04eaeee9d5a
jag114 <jag200202@gmail.com>
2023-11-09 12:09:06 +0100

    commit

---
+++ UserData.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spottyfy
+{
+    public class UserData
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        //[BsonElement("basic-info")]
+        public string login { get; set; }
+
+        public string password { get; set; }
+
+        public string rank { get; set; }
+
+        public string creationDate { get; set; }
+    }
+}
